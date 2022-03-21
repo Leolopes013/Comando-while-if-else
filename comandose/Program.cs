86909ec8bed995df23fd6da64af8503de515ab1d
Leolopes013 <leo_piula@hotmail.com>
2022-03-20 22:55:34 -0300

    comandos basicos

---
+++ comandose/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comandose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Informe um número...: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0)
+            {
+                //numero = numero - 2;
+                numero -= 2;
+            }
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+            else
+            {
+                Console.WriteLine("Número impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
